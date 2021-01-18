b8e9663cb4996939e61a3d2e7daf308ae5adc9c3
greyhexy <whatoget@gmail.com>
2021-01-18 16:29:55 +0800

    finish Day 2: Password Philosophy

---
+++ 2020/day-2/solution.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+file = "input.txt"
+lines = [line.rstrip() for line in open(file).readlines()]
+
+def preprocess() -> int:
+    data = [] 
+    for line in lines:
+        policy, target, passwd = line.split(sep=' ')
+        policy = tuple(map(int, policy.split('-')))
+        target = target.rstrip(':')
+        data.append((policy, target, passwd))
+    return data
+
+def part1(info) -> int:
+    ans = 0
+    for policy, target, passwd in info:
+        min_cnt, max_cnt = policy
+        cnt = passwd.count(target)
+        cond = cnt in range(min_cnt, max_cnt+1)
+        if (cond): ans += 1
+    return ans
+
+def part2(info) -> int:
+    ans = 0
+    for policy, target, passwd in info:
+        pos1, pos2 = policy
+        pos1 -= 1; pos2 -= 1
+        cond0 = pos1 < len(passwd) and pos2 < len(passwd)
+        cond1 = (passwd[pos1] == target) ^ (passwd[pos2] == target)
+        if (cond0 and cond1): ans+= 1
+    return ans
+
+if __name__ == "__main__":
+    data = preprocess()
+    print(f"part 1 ans is {part1(data)}")
+    print(f"part 2 ans is {part2(data)}")
